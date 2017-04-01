7a89f51cea9722717dee21d3250524351a487628
Andressa Sivolella <andressa.sivolella@twistsystems.com>
2017-04-01 12:42:57 -0300

    add eslint and replace tab by 2 spaces

--- src/dog.js
+++ src/dog.js
@@ -1,11 +1,11 @@
 class Dog {
-	constructor(name){
-		this.name = name
-	}
+  constructor(name) {
+    this.name = name
+  }
 
-	bark(){
-		return `My name is ${this.name}`
-	}
+  bark() {
+    return `My name is ${this.name}`
+  }
 }
 
 export default Dog
